311e2e8c03adebe7a92ac2e5be9bfa5a6d41f43c
Fernando Ferreira <fernandoferreira67@hotmail.com>
2026-02-01 19:17:54 -0300

    feat: validation in the components

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router'
+import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
 import { Button } from '@/components/ui/button'
@@ -12,6 +13,36 @@ import {
 } from '@/components/ui/card'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
+
+const signupSchema = z.object({
+  firstName: z.string().trim().min(1, {
+    message: 'O nome é obrigatório',
+  }),
+  lastName: z.string().trim().min(1, {
+    message: 'O sobrenome é obrigatório',
+  }),
+  email: z
+    .string()
+    .email({
+      message: 'o e-mail é obrigatório',
+    })
+    .trim()
+    .min(1, {
+      message: 'O e-mail é obrogatório',
+    }),
+  password: z.string().trim().min({
+    message: 'A senha deve ter no mínimo 6 caracteres',
+  }),
+  passwordConfirmation: z.string().trim().min({
+    message: 'A confirmação de senha é obrigatória',
+  }),
+
+  terms: z.boolean().refine((value) => value == true, {
+    message: 'Você precisa aceitar os termos de uso.',
+  }),
+})
+
+console.log(signupSchema)
 
 const SignupPage = () => {
   return (
