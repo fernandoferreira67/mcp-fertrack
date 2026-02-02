9c4a6028994989bd32f1411f3fcd6af7ce2fcb13
Fernando Ferreira <fernandoferreira67@hotmail.com>
2026-02-02 16:08:32 -0300

    feat: add api integration on signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -23,6 +26,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { api } from '@/lib/axios'
 
 const signupSchema = z.object({
   firstName: z.string().trim().min(1, {
@@ -52,6 +56,20 @@ const signupSchema = z.object({
 })
 
 const SignupPage = () => {
+  const [user, setUser] = useState(null)
+  const signupMutation = useMutation({
+    mutationKey: ['signup'],
+    mutationFn: async (variables) => {
+      const response = await api.post('/users', {
+        first_name: variables.firstName,
+        last_name: variables.lastName,
+        email: variables.email,
+        password: variables.password,
+      })
+      return response.data
+    },
+  })
+
   const form = useForm({
     resolver: zodResolver(signupSchema),
     defaultValues: {
@@ -65,7 +83,22 @@ const SignupPage = () => {
   })
 
   const handleSubmit = (data) => {
-    console.log(data)
+    signupMutation.mutate(data, {
+      onSuccess: (createdUser) => {
+        const accessToken = createdUser.tokens.accessToken
+        const refreshToken = createdUser.tokens.refreshToken
+        setUser(createdUser)
+        localStorage.setItem('accessToken', accessToken)
+        localStorage.setitem('refreshToken', refreshToken)
+        toast.success('Conta criada com successo!')
+      },
+      onError: () => {
+        toast.error('Erro ao criar conta. Por favor tente novamente!')
+      },
+    })
+  }
+  if (user) {
+    return <h1>Olá, {user.first_name}</h1>
   }
 
   return (
