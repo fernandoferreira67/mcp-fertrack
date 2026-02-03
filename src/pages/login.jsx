4d1160b0df7de0a7768aaf695d10a9ea499064ef
Fernando Ferreira <fernandoferreira67@hotmail.com>
2026-02-03 14:47:46 -0300

    feat: add initializing state to auth context

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router'
+import { Link, Navigate } from 'react-router'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -40,7 +40,7 @@ const loginSchema = z.object({
 })
 
 const LoginPage = () => {
-  const { user, login } = useAuthContext()
+  const { user, login, isInitializing } = useAuthContext()
 
   const form = useForm({
     resolver: zodResolver(loginSchema),
@@ -52,13 +52,10 @@ const LoginPage = () => {
 
   const handleSubmit = (data) => login(data)
 
+  if (isInitializing) return null
+
   if (user) {
-    return (
-      <div className="flex items-center justify-center text-4xl font-bold">
-        {' '}
-        Olá {user.first_name}
-      </div>
-    )
+    return <Navigate to="/" />
   }
 
   return (
