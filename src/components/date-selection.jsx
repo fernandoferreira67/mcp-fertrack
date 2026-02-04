ebba1a6eb034d3e5c66bc46ef3004ea4bab760b5
Fernando Ferreira <fernandoferreira67@hotmail.com>
2026-02-04 14:56:39 -0300

    feat: ensure user balance is shown on home page

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,15 +1,20 @@
+import { useQueryClient } from '@tanstack/react-query'
 import { addMonths } from 'date-fns'
 import { format } from 'date-fns'
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
+
+import { useAuthContext } from '@/contexts/auth'
 
 import { DatePickerWithRange } from './ui/date-picker-with-range'
 
 const formatDateToQueryParam = (date) => format(date, 'yyyy-MM-dd')
 
 const DateSelection = () => {
+  const queryClient = useQueryClient()
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
+  const { user } = useAuthContext()
   const [date, setDate] = useState({
     // 2. quando eu recarregar a página, eu pego o from e to da url e persisto eles no state
     // Senão tiver parametros newDate()
@@ -28,7 +33,10 @@ const DateSelection = () => {
     queryParams.set('from', formatDateToQueryParam(date.from))
     queryParams.set('to', formatDateToQueryParam(date.to))
     navigate(`/?${queryParams.toString()}`)
-  }, [navigate, date])
+    queryClient.invalidateQueries({
+      queryKey: ['balance', user.id],
+    })
+  }, [navigate, date, queryClient, user.id])
 
   return <DatePickerWithRange value={date} onChange={setDate} />
 }
